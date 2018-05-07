eb47347096fd7a4244bb0b4cb4b67099fd47cc7d
meihao1203 <meihao19931203@outlook.com>
2018-05-07 19:02:32 +0800

    C++ learning

---
+++ 05072018/tcp/server/TcpConnection.h
@@ -0,0 +1,18 @@
+ ///
+ /// @file    TcpConnection.h
+ /// @author  meihao1203(meihao19931203@outlook.com)
+ /// @date    2018-05-07 19:00:04
+ ///
+ 
+#ifndef __TCPCONNECTION_H__
+#define __TCPCONNECTION_H__
+#include<iostream>
+using namespace std;
+namespace meihao
+{
+	class TcpConnection::public::enable_shared_from_this<TcpConnection>
+	{
+		
+	};
+};
+#endif
