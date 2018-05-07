a29bbb981c21e3e89ca03095825a8de4208d417f
meihao1203 <meihao19931203@outlook.com>
2018-05-07 09:45:36 +0800

    C++ learning

---
+++ 05072018/tcp/server/Socket.cpp
@@ -0,0 +1,32 @@
+ ///
+ /// @file    Socket.cpp
+ /// @author  meihao1203(meihao19931203@outlook.com)
+ /// @date    2018-05-06 20:51:21
+ ///
+ 
+#include"Socket.h"
+#include<iostream>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<errno.h>
+#include<unistd.h>
+using namespace std;
+#define handle_error(msg) \
+	do{\
+		perror(msg);\
+		exit(-1);\
+	}while(0);
+namespace meihao
+{
+	int getSocketfd()
+	{
+		int sfd = socket(AF_INET,SOCK_STREAM,0);
+		if(-1==sfd)
+		{
+			handle_error("socket");
+		}
+	}
+	Socket::Socket():_fd(getSocketfd())
+	{
+	}
+};
