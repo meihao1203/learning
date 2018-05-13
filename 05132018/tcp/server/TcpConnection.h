0336200b8821ff16557bbc34c5efbd70d28f5a7f
meihao1203 <meihao19931203@outlook.com>
2018-05-13 15:24:54 +0800

    C++ learning

--- 05132018/tcp/server/TcpConnection.h
+++ 05132018/tcp/server/TcpConnection.h
@@ -17,12 +17,12 @@ namespace meihao
 	class TcpConnection;
 	typedef std::shared_ptr<TcpConnection> TcpConnectionPtr;
 	//利用智能共享指针托管TcpConnection类型指针，这样不用考虑最后的资源释放
+	typedef function<void(const TcpConnectionPtr& conn)> TcpConnectionCallback;   // 这里要为public，不然后面的Epoll类访问不到,最开始我放到类里面了，放到外面访问更方便
+	//function函数对象绑定回调函数
 	class TcpConnection:public std::enable_shared_from_this<TcpConnection>
 	{
 		//继承enable_shared_from_this,使用里面的shared_from_this()方法传递指向函数
 		//本身的指针，防止出现shared_ptr误用造成多次释放或者拷贝
-		typedef function<void(const TcpConnectionPtr& conn)> TcpConnectionCallback;
-		//function函数对象绑定回调函数
 		public:
 			TcpConnection(int confd);  // 连接到的fd
 			~TcpConnection();
