22570c0f7ed17b73a01cc1e038cd13053d7a8843
meihao1203 <meihao19931203@outlook.com>
2018-05-15 14:07:45 +0800

    C++ learning

---
+++ 05152018/test.cpp
@@ -0,0 +1,13 @@
+ ///
+ /// @file    test.cpp
+ /// @author  meihao1203(meihao19931203@outlook.com)
+ /// @date    2018-05-15 14:05:50
+ ///
+ 
+//命令行把cout结果利用>或者>>重定向到log.tx
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"hello,world!"<<endl;
+}
