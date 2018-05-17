f771cc422ba41a57cf2700e523c4917da90682aa
meihao1203 <meihao19931203@outlook.com>
2018-05-17 16:18:20 +0800

    C++ learning

--- 04192018/threadPool/test.cpp
+++ 04192018/threadPool/test.cpp
@@ -7,6 +7,7 @@
 #include<iostream>
 #include"Threadpool.h"
 #include<unistd.h>
+#include<stdlib.h>
 using namespace std;
 class MyTask:public meihao::Task
 {
@@ -27,7 +28,7 @@ int main()
 	while(cnt--)
 	{
 		threadpool.addTask(ptask);  // 线程池中添加任务
-		sleep(1);
+		//sleep(1);
 	}
 	threadpool.stop();
 	return 0;
