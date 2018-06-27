d93126a38a274c4d7d04809cbe5ddf0be04ca076
meihao1203 <meihao19931203@outlook.com>
2018-06-27 21:32:14 +0800

    C++ learning

--- 06272018/BFS.h
+++
@@ -1,8 +0,0 @@
-#ifndef __BFS_H__
-#define __BFS_H__
-#include"Graph.h"
-namespace meihao
-{
-	void BFSTraversal(const meihao::Graph& g);
-};
-#endif
--- 06272018/DFS.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DFS_H__
-#define __DFS_H__
-#include"Graph.h"
-namespace meihao
-{
-	void DFS(const meihao::Graph& g,int vi,bool*& visited);  //参数，图和顶点数组中某个顶点的下标
-	void DFSTraversal(const meihao::Graph& g);
-};
-#endif
