88feae3a4b753f8861c68d9ebdb90f217708eaef
meihao1203 <meihao19931203@outlook.com>
2018-06-29 16:41:32 +0800

    C++ learning

--- 06262018/Graph/Graph.cpp
+++ 06262018/Graph/Graph.cpp
@@ -82,7 +82,7 @@ namespace meihao
 	}
 	weight_vaule_type Graph::getGraphEdgeWeight(int vi,int vj)const
 	{
-		if(vi<0||vj<0||vi==vj||vi>=_vertexNum||vi>=_vertexNum)
+		if(vi<0||vj<0||vi==vj||vi>=_vertexNum||vj>=_vertexNum)
 		{
 			graph_error = true;
 			return -1;   //因为图的权值可以是负数，为了区分，就拿一个bool变量来标记什么情况下-1表示出错了
@@ -91,7 +91,7 @@ namespace meihao
 	}
 	int Graph::setGraphEdgeWeight(int vi,int vj,weight_vaule_type weight)
 	{
-		if(vi<0||vj<0||vi==vj||vi>=_vertexNum||vi>=_vertexNum||weight>max_weight_value_type)
+		if(vi<0||vj<0||vi==vj||vi>=_vertexNum||vj>=_vertexNum||weight>max_weight_value_type)
 		{
 			graph_error = true;
 			return -1;   
