c2041b4fc374c77fe114e4e8208133a6d10f3f17
meihao1203 <meihao19931203@outlook.com>
2018-07-01 17:23:18 +0800

    C++ learning

---
+++ 07012018/Graph_拓扑排序_AOV/testmain.cpp
@@ -0,0 +1,14 @@
+﻿#include"AOV.h"
+#include"Graph.h"
+#include<iostream>
+using namespace std;
+int main()
+{
+	meihao::Graph g("data.txt");
+	int ret = meihao::TopologicalSort_AOV(g);
+	if(0==ret)
+		cout<<"success!"<<endl;
+	else
+		cout<<"fail!"<<endl;
+}
+//3 1 2 6 0 4 5 8 7 12 9 10 13 11 success!
