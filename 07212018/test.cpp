f19bbb552707c4914ff8ba781111756fd6e5572d
meihao1203 <meihao19931203@outlook.com>
2018-07-21 10:12:13 +0800

    C++ learning

---
+++ 07212018/test.cpp
@@ -0,0 +1,17 @@
+ ///
+ /// @file    test.cpp
+ /// @author  meihao1203(meihao19931203@outlook.com)
+ /// @date    2018-07-21 10:11:02
+ ///
+ 
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a = 1;
+	int* ptr = NULL;
+	cout<<sizeof(a)<<endl;
+	cout<<sizeof(ptr)<<endl;
+}
+//4
+//8
