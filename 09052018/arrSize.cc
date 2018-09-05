7e0d503a07fa7860f557ed6271bc6f2349bb3ae7
meihao1203 <meihao19931203@outlook.com>
2018-09-05 09:07:07 +0800

    C++ learning

---
+++ 09052018/arrSize.cc
@@ -0,0 +1,15 @@
+ ///
+ /// @file    arrSize.cc
+ /// @author  meihao1203(meihao19931203@outlook.com)
+ /// @date    2018-09-05 09:03:39
+ ///
+ 
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+	char buf[10] = "hello";
+	cout<<sizeof(buf)<<" "<<strlen(buf)<<endl;
+}
+//10 5
